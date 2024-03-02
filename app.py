0f5295d16d00ea651fefe2ec1c595e08f43aade4
eashan98 <eashan1998raina@gmail.com>
2024-03-02 23:06:34 +0530

    all detection completed with loading on it

--- app.py
+++ app.py
@@ -5,15 +5,40 @@ from io import BytesIO
 from werkzeug.utils import secure_filename
 import os
 from datetime import datetime
+from optparse import OptionParser
 
 import double_jpeg_compression
 import image_extraction
 import image_meta_data_extraction
+import cfa_artifact
+import noise_inconsistency
+import error_level_analysis
+import string_extraction
+import copy_move_detection
 
 app = Flask(__name__)
 
 app.config['INPUT_DIR'] = 'static/input/images'
 app.config['OUTPUT_DIR'] = 'static/output/images'
+
+cmd = OptionParser("usage: %prog image_file [options]")
+cmd.add_option('', '--imauto',
+               help='Automatically search identical regions. (default: %default)', default=1)
+cmd.add_option('', '--imblev',
+               help='Blur level for degrading image details. (default: %default)', default=8)
+cmd.add_option('', '--impalred',
+               help='Image palette reduction factor. (default: %default)', default=15)
+cmd.add_option(
+    '', '--rgsim', help='Region similarity threshold. (default: %default)', default=5)
+cmd.add_option(
+    '', '--rgsize', help='Region size threshold. (default: %default)', default=1.5)
+cmd.add_option(
+    '', '--blsim', help='Block similarity threshold. (default: %default)', default=200)
+cmd.add_option('', '--blcoldev',
+               help='Block color deviation threshold. (default: %default)', default=0.2)
+cmd.add_option(
+    '', '--blint', help='Block intersection threshold. (default: %default)', default=0.2)
+opt, args = cmd.parse_args()
 
 @app.route('/')
 def home():
@@ -31,12 +56,12 @@ def history():
 def processCompressDetection():
     try:
         if 'image' not in request.files:
-            return jsonify({'status': False, 'message': 'No image file provided'})
+            return jsonify({'status': False, 'type':'danger', 'message': 'No Image Selected', 'data': 'No image file provided!'})
 
         file = request.files['image']
         
         if file.filename == '':
-            return jsonify({'status': False, 'message': 'Invalid Image File.'})
+            return jsonify({'status': False, 'type':'danger', 'message': 'Invalid Image', 'data': 'Invalid image file!'})
                 
         original_filename = secure_filename(file.filename)
 
@@ -48,24 +73,24 @@ def processCompressDetection():
         double_compressed = double_jpeg_compression.detect(app.config['INPUT_DIR'],app.config['OUTPUT_DIR'], filename)
         
         if double_compressed:
-            return jsonify({'status': True, 'message': 'The image is double compressed.'})
+            return jsonify({'status': True, 'type':'danger', 'message': 'Double Compression Found', 'data': 'The image is double compressed!'})
         else:
-            return jsonify({'status': False, 'message': 'The image is single compressed.'})
+            return jsonify({'status': False, 'type':'success', 'message': 'Single Compression Found', 'data': 'The image is single compressed!'})
 
     except Exception as e:
-        return jsonify({'status': False, 'message': str(e)})
+        return jsonify({'status': False, 'type':'danger', 'message': "Error Exception", 'data': str(e)})
     
 
 @app.route('/processMetaDataAnalysis', methods=['POST'])
 def processMetaDataAnalysis():
     try:
         if 'image' not in request.files:
-            return jsonify({'status': False, 'message': 'No image file provided'})
+            return jsonify({'status': False, 'type':'danger', 'message': 'No Image Selected', 'data': 'No image file provided!'})
 
         file = request.files['image']
         
         if file.filename == '':
-            return jsonify({'status': False, 'message': 'Invalid Image File.'})
+            return jsonify({'status': False, 'type':'danger', 'message': 'Invalid Image', 'data': 'Invalid image file!'})
                 
         original_filename = secure_filename(file.filename)
 
@@ -75,28 +100,110 @@ def processMetaDataAnalysis():
         file.save(os.path.join(app.config['INPUT_DIR'], filename))
                 
         image_meta_data_extracted = image_meta_data_extraction.detect(app.config['INPUT_DIR'],app.config['OUTPUT_DIR'], filename)
-        
-        print(image_meta_data_extracted)
-        
+                
         if image_meta_data_extracted:
-            return jsonify({'status': True, 'message': image_meta_data_extracted})
+            return jsonify({'status': True, 'type':'success', 'message': 'Meta Data Found', 'data': image_meta_data_extracted})
         else:
-            return jsonify({'status': False, 'message': 'No Mata Data Associated With Image.'})
+            return jsonify({'status': True, 'type':'danger', 'message': 'No Mata Data Found', 'data': 'No mata data found associated with image!'})
 
     except Exception as e:
-        return jsonify({'status': False, 'message': str(e)})
+        return jsonify({'status': False, 'type':'danger', 'message': "Error Exception", 'data': str(e)})
     
+    
+@app.route('/processCfaArtifactDetection', methods=['POST'])
+def processCfaArtifactDetection():
+    try:
+        if 'image' not in request.files:
+            return jsonify({'status': False, 'type':'danger', 'message': 'No Image Selected', 'data': 'No image file provided!'})
+
+        file = request.files['image']
+        
+        if file.filename == '':
+            return jsonify({'status': False, 'type':'danger', 'message': 'Invalid Image', 'data': 'Invalid image file!'})
+                
+        original_filename = secure_filename(file.filename)
+
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        filename = f"{timestamp}_{original_filename}"
+        
+        file.save(os.path.join(app.config['INPUT_DIR'], filename))
+                
+        image_cfa_artifact = cfa_artifact.detect(app.config['INPUT_DIR'],app.config['OUTPUT_DIR'], filename, opt, args)
+                
+        if image_cfa_artifact:
+            return jsonify({'status': True, 'type':'danger', 'message': 'CFA Artifacts Detected', 'data': str(image_cfa_artifact) + ' CFA artifacts detected!', 'image': os.path.join(app.config['OUTPUT_DIR'], filename)})
+        else:
+            return jsonify({'status': True, 'type':'success', 'message': 'No CFA Artifacts Detected', 'data': 'No CFA artifacts detected!', 'image': os.path.join(app.config['OUTPUT_DIR'], filename)})
+
+    except Exception as e:
+        return jsonify({'status': False, 'type':'danger', 'message': "Error Exception", 'data': str(e)})
+    
+@app.route('/processNoiseInconsistency', methods=['POST'])
+def processNoiseInconsistency():
+    try:
+        if 'image' not in request.files:
+            return jsonify({'status': False, 'type':'danger', 'message': 'No Image Selected', 'data': 'No image file provided!'})
+
+        file = request.files['image']
+        
+        if file.filename == '':
+            return jsonify({'status': False, 'type':'danger', 'message': 'Invalid Image', 'data': 'Invalid image file!'})
+                
+        original_filename = secure_filename(file.filename)
+
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        filename = f"{timestamp}_{original_filename}"
+        
+        file.save(os.path.join(app.config['INPUT_DIR'], filename))
+                
+        image_noise_inconsistency = noise_inconsistency.detect(app.config['INPUT_DIR'],app.config['OUTPUT_DIR'], filename)
+                
+        if image_noise_inconsistency:
+            return jsonify({'status': True, 'type':'danger', 'message': 'Noise Inconsistency Found', 'data': 'Noise inconsistency found!'})
+        else:
+            return jsonify({'status': True, 'type':'success', 'message': 'No Noise Inconsistency Found', 'data': 'No noise inconsistency found with image!'})
+
+    except Exception as e:
+        return jsonify({'status': False, 'type':'danger', 'message': "Error Exception", 'data': str(e)})
+    
+@app.route('/processErrorLevelAnalysis', methods=['POST'])
+def processErrorLevelAnalysis():
+    try:
+        if 'image' not in request.files:
+            return jsonify({'status': False, 'type':'danger', 'message': 'No Image Selected', 'data': 'No image file provided!'})
+
+        file = request.files['image']
+        
+        if file.filename == '':
+            return jsonify({'status': False, 'type':'danger', 'message': 'Invalid Image', 'data': 'Invalid image file!'})
+                
+        original_filename = secure_filename(file.filename)
+
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        filename = f"{timestamp}_{original_filename}"
+        
+        file.save(os.path.join(app.config['INPUT_DIR'], filename))
+                
+        image_error_level_analysis = error_level_analysis.detect(app.config['INPUT_DIR'],app.config['OUTPUT_DIR'], filename)
+                
+        if image_error_level_analysis:
+            return jsonify({'status': True, 'type':'success', 'message': 'Image Error Level Analysis Completed', 'data': 'Image error level analysis completed successfuly!', 'image': os.path.join(app.config['OUTPUT_DIR'], filename)})
+        else:
+            return jsonify({'status': False, 'type':'danger', 'message': 'Image Error Level Analysis Failed', 'data': 'Image error level analysis failed due to some error!'})
+
+    except Exception as e:
+        return jsonify({'status': False, 'type':'danger', 'message': "Error Exception", 'data': str(e)})
 
 @app.route('/processImageExtraction', methods=['POST'])
 def processImageExtraction():
     try:
         if 'image' not in request.files:
-            return jsonify({'status': False, 'message': 'No image file provided'})
+            return jsonify({'status': False, 'type':'danger', 'message': 'No Image Selected', 'data': 'No image file provided!'})
 
         file = request.files['image']
         
         if file.filename == '':
-            return jsonify({'status': False, 'message': 'Invalid Image File.'})
+            return jsonify({'status': False, 'type':'danger', 'message': 'Invalid Image', 'data': 'Invalid image file!'})
                 
         original_filename = secure_filename(file.filename)
 
@@ -108,12 +215,68 @@ def processImageExtraction():
         image_extracted = image_extraction.detect(app.config['INPUT_DIR'],app.config['OUTPUT_DIR'], filename)
         
         if image_extracted:
-            return jsonify({'status': True, 'message': 'Image extracted successfully.', 'image': os.path.join(app.config['OUTPUT_DIR'], filename)})
+            return jsonify({'status': True, 'type':'success', 'message': 'Image Extracted Successfuly', 'data': 'Image extracted successfuly!', 'image': os.path.join(app.config['OUTPUT_DIR'], filename)})
         else:
-            return jsonify({'status': False, 'message': 'Image extraction failed due to some error.'})
+            return jsonify({'status': False, 'type':'danger', 'message': 'Image Extraction Failed', 'data': 'Image extraction failed due to some error!'})
 
     except Exception as e:
-        return jsonify({'status': False, 'message': str(e)})
+        return jsonify({'status': False, 'type':'danger', 'message': "Error Exception", 'data': str(e)})
+    
+@app.route('/processStringExtraction', methods=['POST'])
+def processStringExtraction():
+    try:
+        if 'image' not in request.files:
+            return jsonify({'status': False, 'type':'danger', 'message': 'No Image Selected', 'data': 'No image file provided!'})
+
+        file = request.files['image']
+        
+        if file.filename == '':
+            return jsonify({'status': False, 'type':'danger', 'message': 'Invalid Image', 'data': 'Invalid image file!'})
+                
+        original_filename = secure_filename(file.filename)
+
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        filename = f"{timestamp}_{original_filename}"
+        
+        file.save(os.path.join(app.config['INPUT_DIR'], filename))
+                
+        image_string_extraction = string_extraction.detect(app.config['INPUT_DIR'],app.config['OUTPUT_DIR'], filename)
+                
+        if image_string_extraction:
+            return jsonify({'status': True, 'type':'success', 'message': "Image String Extracted", 'data': image_string_extraction})
+        else:
+            return jsonify({'status': False, 'type':'danger', 'message': 'Image String Extraction Failed', 'data': 'Image string extraction failed due to some error!'})
+
+    except Exception as e:
+        return jsonify({'status': False, 'type':'danger', 'message': "Error Exception", 'data': str(e)})
+    
+@app.route('/processCopyMoveDetection', methods=['POST'])
+def processCopyMoveDetection():
+    try:
+        if 'image' not in request.files:
+            return jsonify({'status': False, 'type':'danger', 'message': 'No Image Selected', 'data': 'No image file provided!'})
+
+        file = request.files['image']
+        
+        if file.filename == '':
+            return jsonify({'status': False, 'type':'danger', 'message': 'Invalid Image', 'data': 'Invalid image file!'})
+                
+        original_filename = secure_filename(file.filename)
+
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        filename = f"{timestamp}_{original_filename}"
+        
+        file.save(os.path.join(app.config['INPUT_DIR'], filename))
+                
+        image_copy_move_detection = copy_move_detection.detect(app.config['INPUT_DIR'],app.config['OUTPUT_DIR'], filename)
+        
+        if image_copy_move_detection:
+            return jsonify({'status': True, 'type':'danger', 'message': 'Copy Move Forgery Detected', 'data': 'Copy move forgery detected in image!', 'image': os.path.join(app.config['OUTPUT_DIR'], filename)})
+        else:
+            return jsonify({'status': True, 'type':'success', 'message': 'No Copy Move Detected', 'data': 'No copy move detection found!', 'image': os.path.join(app.config['INPUT_DIR'], filename)})
+
+    except Exception as e:
+        return jsonify({'status': False, 'type':'danger', 'message': "Error Exception", 'data': str(e)})
 
 
 if __name__ == '__main__':
--- static/js/script.js
+++ static/js/script.js
@@ -5,6 +5,34 @@ function handleImageUpload() {
     var previewImage = document.getElementById('preview_image');
     var fileNameElement = document.getElementById('file_name');
 
+    const output_content = document.getElementById('output_content');
+    const output_image = document.getElementById('output_image');
+    const output_title = document.getElementById('output_title');
+    const output_content_heading = document.getElementById('output_content_heading');
+    const output_content_subheading = document.getElementById('output_content_subheading');
+    const preview_output_image = document.getElementById('preview_output_image');
+
+    const buttons = document.querySelectorAll('.detection-button');
+    buttons.forEach(function(button){
+        button.classList.remove('active');
+        const noLoadingImg = button.querySelector('.icon > .no-loading');
+        const loadingImg = button.querySelector('.icon > .loading');
+        noLoadingImg?.classList.remove('d-none');
+        loadingImg?.classList.add('d-none');
+    });
+
+    output_content.style.display = 'block';
+    output_image.style.display = 'none';
+    output_image.classList.remove('w-full');
+    preview_output_image.src = '';
+    output_title.textContent = 'Output';
+    output_content_heading.textContent = 'Output will be shown here!';
+    output_content_subheading.textContent = 'Please select any option to see output';
+    output_title.classList.remove('text-success');
+    output_title.classList.remove('text-info');
+    output_title.classList.remove('text-danger');
+
+
     if (input.files && input.files[0]) {
         var reader = new FileReader();
 
@@ -29,23 +57,36 @@ function forgeryDetection(detectionType) {
     const fileInput = document.getElementById('file_upload');
     const output_content = document.getElementById('output_content');
     const output_image = document.getElementById('output_image');
+    const output_title = document.getElementById('output_title');
     const output_content_heading = document.getElementById('output_content_heading');
     const output_content_subheading = document.getElementById('output_content_subheading');
     const detection_button = document.getElementById(detectionType);
-    var noLoadingImg = detection_button.querySelector('.icon > .no-loading');
-    var loadingImg = detection_button.querySelector('.icon > .loading');
+    const preview_output_image = document.getElementById('preview_output_image');
+    const noLoadingImg = detection_button.querySelector('.icon > .no-loading');
+    const loadingImg = detection_button.querySelector('.icon > .loading');
     const file = fileInput.files[0];
-
-    var buttons = document.querySelectorAll('.detection-button');
+    
+    const buttons = document.querySelectorAll('.detection-button');
     buttons.forEach(function(button){
         if(button.id != detectionType){
             button.classList.remove('active');
         }
     });
+
     detection_button.classList.add('active');
     noLoadingImg?.classList.add('d-none');
     loadingImg?.classList.remove('d-none');
     detection_button.disabled = true;
+    output_content.style.display = 'none';
+    output_image.style.display = 'block';
+    output_image.classList.add('w-full');
+    preview_output_image.src = 'static/images/gif/loading-200.gif';
+    preview_output_image.style.display = 'block';
+    output_title.classList.remove('text-success');
+    output_title.classList.remove('text-info');
+    output_title.classList.remove('text-danger');
+    output_title.classList.add('text-info');
+    output_title.textContent = 'Processing Image...';
 
     if (!file) {
         alert('Please select an image file.');
@@ -67,27 +108,54 @@ function forgeryDetection(detectionType) {
         detection_button.disabled = false;
         noLoadingImg?.classList.remove('d-none');
         loadingImg?.classList.add('d-none');
+        output_title.classList.remove('text-success');
+        output_title.classList.remove('text-info');
+        output_title.classList.remove('text-danger');
         if(data.status){
-            output_content_heading.innerHTML = data.message;
+            output_content_heading.innerHTML = data.data;
             output_content_subheading.textContent = 'Image Processed Successfully';
             if(data.image){
+                output_title.classList.add(data.type == 'success' ? 'text-success' : (data.type == 'info' ? 'text-info' : 'text-danger'));
+                output_title.textContent = data.message;
                 output_content.style.display = 'none';
                 output_image.style.display = 'block';
-                let preview_output_image = document.getElementById('preview_output_image');
+                output_image.classList.add('w-full');
                 preview_output_image.src = data.image;
+                preview_output_image.style.display = 'block';
             }else{
+                output_title.classList.add(data.type == 'success' ? 'text-success' : (data.type == 'info' ? 'text-info' : 'text-danger'));
+                output_title.textContent = data.message;
                 output_content.style.display = 'block';
                 output_image.style.display = 'none';
+                output_image.classList.remove('w-full');
             }
             return;
         }else{
-            output_content_heading.textContent = 'Error: ' + data.message;
+            output_title.classList.add(data.type == 'success' ? 'text-success' : (data.type == 'info' ? 'text-info' : 'text-danger'));
+            output_title.textContent = 'Error: ' + data.message;
+            output_content.style.display = 'block';
+            output_image.style.display = 'none';
+            output_image.classList.remove('w-full');
+            output_content_heading.textContent = 'Error: ' + data.data;
             output_content_subheading.textContent = 'Some Error Occurred';
             return;
         }
     })
     .catch(error => {
         console.error('Error:', error);
+        output_title.classList.remove('text-success');
+        output_title.classList.remove('text-info');
+        output_title.classList.remove('text-danger');
+        output_title.classList.add('text-danger');
+        output_title.textContent = 'Error : Exception Occurred';
+        output_content.style.display = 'block';
+        output_image.style.display = 'none';
+        output_image.classList.remove('w-full');
+        detection_button.disabled = false;
+        noLoadingImg?.classList.remove('d-none');
+        loadingImg?.classList.add('d-none');
+        output_content_heading.textContent = 'Error: ' + error;
+        output_content_subheading.textContent = 'Some Error Occurred';
         alert(error)
     });
 }
